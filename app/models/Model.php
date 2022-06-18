45527381f31fc9c486fc3f1e14bfd938eec9f563
Christian <christiannmenezes@gmail.com>
2022-06-18 18:09:46 -0300

    query methods

--- app/models/Model.php
+++ app/models/Model.php
@@ -17,16 +17,30 @@ abstract class Model
 
   public function all() 
   {
+    $sql = "SELECT * FROM {$this->table}";
+    $list = $this->connection->prepare($sql);
+    $list->execute();
 
+    return $list->fetchAll();
   }
 
-  public function find()
+  public function find($field, $value)
   {
-    
+    $sql = "SELECT * FROM {$this->table} WHERE {$field} = ?";
+    $list = $this->connection->prepare($sql);
+    $list->bindValue(1, $value);
+    $list->execute();
+
+    return $list->fetch();
   }
 
-  public function delete()
+  public function delete($field, $value)
   {
-    
+    $sql = "DELETE FROM {$this->table} WHERE {$field} = ?";
+    $delete = $this->connection->prepare($sql);
+    $delete->bindValue(1, $value);
+    $delete->execute();
+
+    return $delete->rowCount();
   }
 }
